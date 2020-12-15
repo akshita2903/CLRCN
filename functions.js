df3e0c1ad5c3a680aeb61bc6fc181eb5699515bb
akshita2903 <68686051+akshita2903@users.noreply.github.com>
2020-12-15 17:17:05 +0530

    Added files via upload

---
+++ functions.js
@@ -0,0 +1,15 @@
+
+function findcolor()
+{
+    var colorcomb="0123456789ABCDEF";
+var color="#";
+    for(var i=0;i<6;i++)
+    color += colorcomb[(Math.floor(Math.random() *16))];
+    return color;
+}
+function colorchange()
+{
+    var getcolor=findcolor(); //get colorname from function chnge color
+    document.getElementById("hr").innerHTML=getcolor;
+    document.getElementsByTagName("BODY")[0].style.backgroundColor = getcolor;
+}
