3a270c6c9c496491a5061bfb9a9eca33b4566fa0
akshita2903 <68686051+akshita2903@users.noreply.github.com>
2020-12-15 17:24:44 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 
 function findcolor()
 {
-    var colorcomb="0123456789ABCDEF";
+    var colorcomb="0123456789ABCDEF"; 
 var color="#";
     for(var i=0;i<6;i++)
     color += colorcomb[(Math.floor(Math.random() *16))];
@@ -9,7 +9,7 @@ var color="#";
 }
 function colorchange()
 {
-    var getcolor=findcolor(); //get colorname from function chnge color
+    var getcolor=findcolor(); //get colorname from function find color
     document.getElementById("hr").innerHTML=getcolor;
     document.getElementsByTagName("BODY")[0].style.backgroundColor = getcolor;
 }
